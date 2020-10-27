eaca7005e030cf98e209a408fcd93f65cd4877d5
JeffreyZoet <70272880+JeffreyZoet@users.noreply.github.com>
2020-10-27 15:03:40 +0100

    Add files via upload

---
+++ Opdracht24/script.js
@@ -0,0 +1,15 @@
+console.log("Hallo Winc Academy studenten");
+// Dit is een grote som en deze tekst laat ik nu even staan ook al staat er geen code onder.
+/* Dit zijn twee 
+rekenvoorbeelden die een uitkomst geven in de console. */
+let multiplication = 1230941 * 1823794;
+console.log(multiplication);
+let division= 637263 / 54;
+console.log(division);
+
+/* Dit zijn rekenvoorbeelden 
+die geen uitkomst geven in de console
+let abstraction = 100 - 500;
+console.log(abstraction);
+let modules= 10 % 200;
+console.log(modules); */
