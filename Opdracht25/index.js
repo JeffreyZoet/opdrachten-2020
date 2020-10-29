b888b50618c9a0071a1bf67a129eb5830d48da18
JeffreyZoet <70272880+JeffreyZoet@users.noreply.github.com>
2020-10-29 15:22:35 +0100

    Add files via upload

---
+++ Opdracht25/index.js
@@ -0,0 +1,27 @@
+const myAge = 25;
+if (myAge >= 18) {
+   console.log('You may enter the bar')
+}
+else {
+   console.log('You are to young and are not allowed to enter the bar')
+}
+
+const isFemale = true;
+if (person = isFemale) {
+   console.log('You can join the ladiesnight')
+}
+else {
+   console.log('You will have to go to another bar')
+}
+
+const driverStatus = true;
+if (bob = driverStatus) {
+   console.log('The driver is a bob and is allowed to drive the car')
+}
+else {
+   console.log('The driver is not a bob and is not allowed to drive the car')
+}
+
+
+
+
