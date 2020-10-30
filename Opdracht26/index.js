b8ce80c00fee30dc84ec88dfb56b0d8591a3054e
JeffreyZoet <70272880+JeffreyZoet@users.noreply.github.com>
2020-10-30 16:45:36 +0100

    Add files via upload

---
+++ Opdracht26/index.js
@@ -0,0 +1,63 @@
+const age = 18;
+const isFemale = true;
+const driverStatus = 'bob';
+const name = 'Bram';
+const totalAmount = 55;
+
+
+
+if (age >= 18) {
+   console.log('You may enter the bar')
+}
+else {
+   console.log('You are to young and are not allowed to enter the bar')
+}
+
+if (age >= 18 && age <= 25) {
+   console.log('You will receive a discount on your drinks')
+}
+else {
+   console.log('You will not receive a discount on your drinks')
+}
+
+
+if (person = isFemale) {
+   console.log('You can join the ladiesnight')
+}
+else {
+   console.log('You will have to go to another bar')
+}
+
+
+if (driverStatus === 'bob') {
+   console.log('The driver is a bob and is allowed to drive the car')
+}
+else {
+   console.log('The driver is not a bob and is not allowed to drive the car')
+}
+
+
+if (name == 'Sarah' || name == 'Bram') {
+   console.log ('You will receive a free beer')
+}
+else {
+   console.log ('You will have to pay for your beer')
+}
+
+
+if (totalAmount <=25) {
+   console.log ('You will receive nothing')
+}
+if (totalAmount > 25 && totalAmount <=50) {
+   console.log ('You will receive (vegan) snacks')
+}
+if (totalAmount > 50 && totalAmount <100) {
+   console.log ('You will receive a portion of nachos')
+}
+if (totalAmount >= 100) {
+   console.log ('You will a 33cc bottle champagne')
+}
+
+
+
+
